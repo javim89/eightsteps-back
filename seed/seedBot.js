1d7e3c54adab9d83c2353f4848880562c6bbbf6d
Javi <javier.marengo@devbase.us>
2023-11-07 16:10:40 -0300

    fix: add responseTime on seed bot

--- seed/seedBot.js
+++ seed/seedBot.js
@@ -1,20 +1,23 @@
 import axios from "axios";
 import UserBot from "../models/UserBot.js";
 
+const generateRandomNumber = () => Math.floor(Math.random() * 20) + 1;
+
 export default async function seedUserBot() {
   const bots = await UserBot.find();
   const botsToSave = [];
   if (bots.length === 0) {
     for (let i = 0; i < 7; i += 1) {
-      // eslint-disable-next-line no-await-in-loop
-      const response = await axios("https://api.generadordni.es/v2/profiles/person");
-      const { name, surname, username } = response.data[0];
-      const userBot = new UserBot({
-        name,
-        surname,
-        alias: username,
+      axios.get("https://api.generadordni.es/v2/profiles/person").then((response) => {
+        const { name, surname, username } = response.data[0];
+        const userBot = new UserBot({
+          name,
+          surname,
+          alias: username,
+          responseTime: generateRandomNumber(),
+        });
+        botsToSave.push(userBot.save());
       });
-      botsToSave.push(userBot.save());
     }
   }
   await Promise.all(botsToSave);
